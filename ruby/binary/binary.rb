343be5321a8252b152b1bff7c386ee8ffa8be2ec
James Crockett <jecrockett@gmail.com>
2016-05-29 14:17:48 -0600

    Fixed Binary. Refactored. Fixed version test in test file

--- ruby/binary/binary.rb
+++ ruby/binary/binary.rb
@@ -1,15 +1,30 @@
+require 'pry'
+
 class Binary
 
   def initialize(input)
+    raise ArgumentError unless input_is_valid?(input)
     @input = input
     @output = []
   end
 
   def to_decimal
     @input.length.times do |i|
-      @output << (@input[-(i+1)] * 2**i)
+      binary_value = (@input[-(i+1)] * 2**i)
+      add_to_output(binary_value)
     end
-    @output.reduce(:+).to_i
+    @output.reduce(:+)
   end
 
+  def add_to_output(value)
+    value.to_s.chars.each do |num|
+      @output << num.to_i
+    end
+  end
+
+  def input_is_valid?(input)
+    input.chars.all? { |num| num == "0" || num == "1" }
+  end
+
+VERSION = 2
 end
